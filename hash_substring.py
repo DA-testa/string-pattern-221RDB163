e2463e8fd17d965e0dfbd43ea256e275613b86be
221RDB163 <125826072+221RDB163@users.noreply.github.com>
2023-04-17 10:57:43 +0000

    Main

--- hash_substring.py
+++ hash_substring.py
@@ -1,32 +1,68 @@
 # python3
 
 def read_input():
-    # this function needs to aquire input both from keyboard and file
-    # as before, use capital i (input from keyboard) and capital f (input from file) to choose which input type will follow
+    input_type = input()
     
+    if(input_type == "I"):
+        P = input()
+        T = input()
     
-    # after input type choice
-    # read two lines 
-    # first line is pattern 
-    # second line is text in which to look for pattern 
+    if(input_type == "F"):
+        with open(f"tests/{input()}") as file:
+            data = file.readlines()
+            P = data[0]
+            T = data[1]
+            file.close()
     
-    # return both lines in one return
-    
-    # this is the sample return, notice the rstrip function
-    return (input().rstrip(), input().rstrip())
+    return (P.rstrip(), T.rstrip())
 
 def print_occurrences(output):
-    # this function should control output, it doesn't need any return
     print(' '.join(map(str, output)))
 
-def get_occurrences(pattern, text):
-    # this function should find the occurances using Rabin Karp alghoritm 
+def get_hash(pattern : str) -> int:
+    Q, B = 256, 13
 
-    # and return an iterable variable
-    return [0]
+    pattern_length = len(pattern)
+    pattern_hash = 0
 
+    for i in range(pattern_length):
+        unicode_value = get_unicode_pos(pattern[i])
+        pattern_hash = (B * pattern_hash + unicode_value) % Q
 
-# this part launches the functions
+    return pattern_hash
+
+def get_unicode_pos(value):
+    return ord(value)
+
+def get_occurrences(pattern : str, text : str): 
+    Q, B = 256, 13
+
+    pattern_length = len(pattern)
+    text_lenght = len(text)
+
+    multiplayer = 1
+    for i in range(1, pattern_length):
+        multiplayer = (multiplayer * B) % Q
+
+    pattern_hash = get_hash(pattern)
+    text_hash = get_hash(text[:pattern_length])
+
+    positions = list()
+    for symbol_index in range(text_lenght - pattern_length + 1):
+        if(pattern_hash == text_hash):
+            if(text[symbol_index: symbol_index + pattern_length] == pattern):
+                positions.append(symbol_index)
+        
+        if(symbol_index < text_lenght - pattern_length):
+            set_1 = get_unicode_pos(text[symbol_index])
+            set_2 = get_unicode_pos(text[symbol_index + pattern_length])
+
+            text_hash = ((text_hash - set_1 * multiplayer) * B + set_2) % Q
+
+            if(text_hash < 0): text_hash += Q
+
+    return positions
+
 if __name__ == '__main__':
     print_occurrences(get_occurrences(*read_input()))
 
